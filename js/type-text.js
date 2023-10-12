28ef4a4e25686ff19f3e29e7b2b3e69d13ff680c
Oskarmle <121121527+Oskarmle@users.noreply.github.com>
2023-10-12 20:04:25 +0200

    initial commit

---
+++ js/type-text.js
@@ -0,0 +1,6 @@
+const typed = new Typed(".auto-type",  {
+    strings: ["programmerer", "tager billeder", "laver hjemmesider", "er Oskar Eriksen"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: false
+})
